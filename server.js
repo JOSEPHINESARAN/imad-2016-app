19f563c2261f21a3a8396ff1e11b6800a53ad1e0
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-08 10:40:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/favicon.ico', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 
-
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
