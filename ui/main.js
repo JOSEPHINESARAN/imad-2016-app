bd8af90e152d7030b1052d2e0b31f7c03d6789fd
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-08 20:46:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,4 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text'
 );
-elemennt.innerHTML = "New value" ;
+element.innerHTML = "New value" ;
