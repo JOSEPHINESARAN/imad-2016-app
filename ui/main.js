0ebe3985328f9a1caf2b3699e291186eea4f4620
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-10 18:13:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function () {
       //Take some action
       if (request.status === 200){
          //capture a list of names and render it as a list
-    var names = request.response.Text
+    var names = request.response.Text;
     var list = '';
     for (var i=0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>';
