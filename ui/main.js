e6fd77a8620ae618b70265c101fecd3a6ee68f59
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-10 16:09:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ submit.onClick = function (){
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
+        list += '<li>' + names[1] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTMl = list;
