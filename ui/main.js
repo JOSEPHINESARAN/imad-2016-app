ca6ebb0fbe64525e807172e22e4cf9f2006c04ac
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-10 15:50:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,9 +28,15 @@ request.send(null);
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onClick = function () {
+submit.onClick = function (){
     //Make a request to the server and send the name
     
     //capture a list of names and render as a list
-    var names = 
-};
+    var names = ['name1', 'name2','name3'];
+    var list = '';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTMl = list;
+     };
