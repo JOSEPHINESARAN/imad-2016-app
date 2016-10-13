3ff084989e31f9ca3a1f15de0fff94ce14c1e4cd
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-13 06:41:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,26 +3,36 @@ var button = document.getElementById('counter');
 button.onclick = function (){
     
  // Creat a request object
- var request = new XMLHttpRequest();
-  // Capture the response and store it in a variable
- request.onreadystatechange = function (){
-  if (request.readystate === XMLHttpRequest.DONE);{
-      // Take some action
-      if (request.status === 200){
-          var counter = request.responseText;
-       
-          
-         var spaan = document.getElementById('count');
- spaan.innerHTML = counter.toString(); 
-      }
-  }
-   // Not done yet  
- };
+ var request = new XMLHttpRequest;
+
+// Capture the response and store it in a variable
+request.onreadystatechange = function (){
+if (request.readyState === XMLHttpRequest.DONE) {
+//Take some action
+if (request.status === 200){
+//capture a list of names and render it as a list
+var obj = request.responseText;
+var names = JSON.parse(obj);
+var list = '';
+for (var i=0; i list +=');
+
+' + names[i] + '
+'};
+
+var ul = document.getElementById('namelist');
+ul.innerHTML = list};
+};
+// Not done yet
+};
+ 
+ 
+ 
+ 
+ 
  // Make the request
 request.open('GET', 'http://josephinesaran.imad.hasura-app.io/counter', true);
 request.send(null);
- 
-};
+ };
 
 // Submit name
 var submit = document.getElementById('submit.btn');
