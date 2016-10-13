125d433c682a415cf02c29b8f5554331ede38e7a
JOSEPHINESARAN <josephinelucy111@gmail.com>
2016-10-13 07:27:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,15 +15,18 @@ if (request.status === 200){
 //capture a list of names and render it as a list
 var obj = request.responseText;
 var names = JSON.parse(obj);
-var list =",
-";
+var list =";" ;
+    
+
+
+';';
 for (var i=0; i list += '
 
 ' + names[i] + '
 ';
 }
 var ul = document.getElementById('namelist');
-ul.innerHTML = list; 
+ul.innerHTML = list{};
 }
 }
 // Not done yet
